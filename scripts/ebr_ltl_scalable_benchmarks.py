9c9088e61c60f166bbdee3f2240c30b022a05b2c
Shufang Zhu <sei_zsf2010@126.com>
2022-04-06 17:32:25 +0200

    also generate tlsf format

--- scripts/ebr_ltl_scalable_benchmarks.py
+++ scripts/ebr_ltl_scalable_benchmarks.py
@@ -13,10 +13,13 @@ def ebr_ltl_scalable(formula_seeds_folder, save):
     formula_seeds_folder = Path(formula_seeds_folder)
     files = list(formula_seeds_folder.glob("./*.tlsf"))
     for file in files:
-        original_formula = formula_seeds_folder_str+"/"+file.name
+        original_formula_file = formula_seeds_folder_str+"/"+file.name
         formula_file = save_str+"/"+file.parent.stem+"/"+file.stem+".ltl"
         part_file = save_str+"/"+file.parent.stem+"/"+file.stem+".part"
-        cmd = "syfco -f ltl -m fully "+original_formula+" -o "+formula_file+" -pf "+part_file
+        cmd = "syfco -f ltl -m fully "+original_formula_file+" -o "+formula_file+" -pf "+part_file
+        tlsf_formula = Path(original_formula_file).read_text()
+        tlsf_file = save_str+"/"+file.parent.stem+"/"+file.stem+".tlsf"
+        Path(tlsf_file).write_text(tlsf_formula)
         print(cmd)
         os.system(cmd)
 
